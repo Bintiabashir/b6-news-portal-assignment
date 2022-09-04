0c4b20da87fd88156f5a531febfa7092e51c063e
Bintiabashir <bintia.bashir2121@gmail.com>
2022-09-04 23:16:57 +0600

    blog part

--- js/category.js
+++ js/category.js
@@ -133,10 +133,11 @@ function displayBlog() {
     <div tabindex="0"
         class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
         <div class="collapse-title text-xl font-medium">
-        why we use template string in javascript?
+        What is the difference between map,find Filter and forEach in JavaScript?
+
         </div>
         <div class="collapse-content">
-            <p>Template strings are a powerful feature of modern JavaScript released in ES6. It lets us insert/interpolate variables and expressions into strings without needing to concatenate like in older versions of JavaScript. It allows us to create strings that are complex and contain dynamic elements</p>
+            <p>The main difference between forEach and filter is that forEach just loop over the array and executes the callback but filter executes the callback and check its return value.Map like filter & foreach takes a callback and run it against every element on the array but whats makes it unique is it generate a new array based on your existing array. When we want to select a single element from an array ,we use find.</p>
         </div>
     </div>`;
 

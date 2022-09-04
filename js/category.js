46fc34a5bf4d6497a974b30a9a744c50c2456371
Bintiabashir <bintia.bashir2121@gmail.com>
2022-09-04 17:57:28 +0600

    layout done

--- js/category.js
+++ js/category.js
@@ -1,19 +1,61 @@
-const loadCategories = async () => {
+const loadNewsName = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`
     const res = await fetch(url);
     const data = await res.json();
-    displayCategories(data.data.news_category);
+    displayNewsName(data.data.news_category);
 }
 
 
-const displayCategories = categories => {
-    const newsCategories = document.getElementById('news-categories');
-    categories.forEach(category => {
-        const categoriesLi = document.createElement('li');
-        categoriesLi.innerText = `${category.category_name}`
-        newsCategories.appendChild(categoriesLi)
+const displayNewsName = newsNames => {
+    const newsCategoriesName = document.getElementById('news-categories-name');
+    newsNames.forEach(name => {
+        const nameBtn = document.createElement('button');
+        nameBtn.setAttribute('onclick', `loadCategoriesNews('${name.category_id}','${name.category_name}')`);
+        nameBtn.innerText = `${name.category_name}`
+        newsCategoriesName.appendChild(nameBtn)
     });
 
 }
 
-loadCategories();
+const loadCategoriesNews = async (categoryId, categoryName) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategoriesNews(data.data, categoryName);
+}
+
+
+const displayCategoriesNews = (categories, categoryName) => {
+
+
+    const foundedData = document.getElementById('founded-data');
+    foundedData.innerText = `${categories.length} items found for this category ${categoryName}`
+
+
+
+    const newsCategoriesContainer = document.getElementById('news-categories-container');
+    newsCategoriesContainer.textContent = '';
+    categories.forEach(category => {
+        const categoriesDiv = document.createElement('div');
+        categoriesDiv.innerHTML = `<div class="card card-compact bg-base-100 shadow-xl m-7">
+        <img src="${category.image_url}" />
+        <div class="card-body">
+            <h2 class="card-title">${category.title}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+    </div>`
+        newsCategoriesContainer.appendChild(categoriesDiv)
+    });
+
+}
+
+
+
+
+
+
+
+loadNewsName();

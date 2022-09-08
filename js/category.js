f2ddb5dd3a33ddc9e94bd0bf3745db0d577b4621
Bintiabashir <bintia.bashir2121@gmail.com>
2022-09-08 20:17:02 +0600

    modal done

--- js/category.js
+++ js/category.js
@@ -78,7 +78,7 @@ const displayCategoriesNews = (categories, categoryName) => {
         <img src="${category.thumbnail_url}" class="m-5">
         <div class="card-body">
             <h2 class="card-title font-bold">${category.title.length > 100 ? category.title.slice(0, 100) + '...' : category.title}</h2>
-            <p class="text-gray-400">${category.details.length > 500 ? category.details.slice(0, 500) + '...' : category.details}</p>
+            <p class="text-gray-500">${category.details.length > 500 ? category.details.slice(0, 500) + '...' : category.details}</p>
             
             <div class="card-actions justify-between items-center">
             <div class='flex items-center'>
@@ -93,7 +93,7 @@ const displayCategoriesNews = (categories, categoryName) => {
                 <p class="p-2 font-semibold text-zinc-700"> ${category.total_view == null | category.total_view == 0 ? category.total_view = 'no view' : category.total_view}</p>
             </div>
            
-            <label for="my-modal-3" class="btn btn-md btn-primary" onclick="loadNewsDetails('${category._id}')">Show Details</label>
+            <label for="my-modal-3" class="btn btn-md btn-primary" onclick="loadNewsDetails('${category._id}')">Read More</label>
             </div>
         </div>
     </div>`
@@ -125,17 +125,36 @@ const displayNewsDetails = newsDetails => {
         modalBody.innerHTML = ` 
         <img src="${news.image_url}" alt="">
         <h3 class="text-lg font-bold">${news.title}</h3>
-        <p class="py-4">${news.details}</p>
-         <div class="card-actions justify-around items-center">
-            <div class='flex flex-row items-center'>
-            <img src='${news.author.img}'class="w-10 rounded-full h-10 mr-3" alt="">
-                <p> ${news.author.name == null ? news.author.name = 'no author found' : news.author.name}</p>
+        <p class="py-4 text-gray-600">${news.details}</p>
+
+         <div class="card-actions justify-between items-center">
+
+            <div class='flex items-center'>
+               <img src='${news.author.img}'class="w-10 rounded-full h-10 mr-3" alt="">
+
+               <div>
+                 <p class="font-bold text-zinc-600-"> ${news.author.name == null ? news.author.name = 'no author found' : news.author.name}</p>
+                 <p class="text-gray-500"> ${news.author.published_date == null ? news.published_date = 'no data found' : news.author.published_date}</p>
+               </div>
+
             </div>
-            <div class='flex flex-row items-center'>
-            <i class="fa-regular fa-eye"></i>
-                <p class="ml-3"> ${news.total_view == null | news.total_view == 0 ? news.total_view = 'no view' : news.total_view}</p>
+
+            <div class='flex items-center m-1'>
+                <a><i class="fa-regular fa-eye"></i><a/>
+                <p class="p-2 font-bold text-zinc-700"> ${news.total_view == null | news.total_view == 0 ? news.total_view = 'no view' : news.total_view}</p>
             </div>
+
+            <div class='flex items-center'>
+             <div class= "flex" >
+               <p class="p-2 font-semibold text-zinc-700"> ${news.rating.badge}</p>
+               <p class="p-2 font-bold text-zinc-700"> ${news.rating.number}</p>
+             </div>
+             <a>
+               <i class="fa-solid fa-star"></i>
+             <a/>
             </div>
+
+        </div>
         
         `;
 
@@ -184,3 +203,4 @@ function displayBlog() {
 
 loadCategoriesNews('01', 'Breaking News');
 loadNewsName();
+
